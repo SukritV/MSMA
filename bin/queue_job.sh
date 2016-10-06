220d8cc1f2ee3a6da8a5791ea153bbb79c2321ec
Sukrit Venkatagiri <sukrit.v.95@gmail.com>
2016-10-07 00:37:02 +0530

    Changed name

--- bin/queue_job.sh
+++ bin/queue_job.sh
@@ -1,12 +1,13 @@
 #PBS -S /bin/bash
 #PBS -q batch
-#PBS -N MSMA_permute_runs
+#PBS -N MSMA_runs
 #PBS -l nodes=1:ppn=4:AMD
 #PBS -l walltime=48:00:00
 #PBS -l mem=20gb
 #PBS -M sukrit@uga.edu 
 #PBS -m ae
 
-
+cd $PBS_O_WORKDIR
+echo `pwd`
 # CALLS PERMUTE_RUNS.SH SCRIPT 
-./permute_runs.sh
+#./permute_runs.sh
